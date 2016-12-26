49e9b54ff2506bdd9358fba7eb90e5361a723a1c
Leon <leonpaulharris@gmail.com>
2016-12-26 00:48:44 +0000

    first api route, .env

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/', function (Request $request) {
+    return "Hello World";
+})->middleware('auth:api');
