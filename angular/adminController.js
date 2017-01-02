be53070fba80673bcbc44818fe644dc7629fae13
Leon <leonpaulharris@gmail.com>
2017-01-02 15:45:31 +0000

    materialize

--- angular/adminController.js
+++ angular/adminController.js
@@ -6,7 +6,7 @@
 var app =  angular
         .module('StarterApp');
 
-        app.controller('AdminController', function($scope, $http, $mdDialog) {
+        app.controller('AdminController', function($scope, $http) {
 
           $http.get('http://dev.ledfx:8888/testdata/apidata.json').then(function(act) {
               $scope.actdata = act;
@@ -14,5 +14,12 @@ var app =  angular
             })
 
         });
+
+
+
+/**
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
+**/
 
   })(); // EO controller file
